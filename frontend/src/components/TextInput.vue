56742ea765db2f3d70bb2892dba4af9936edd68e
최준원 <cjw1102@naver.com>
2021-09-17 12:34:06 +0900

    feat: TextInput validate error and toggle class

--- frontend/src/components/TextInput.vue
+++ frontend/src/components/TextInput.vue
@@ -5,7 +5,7 @@
         autocomplete="off"
         :type="field.type"
         :value="modelValue"
-        :class="{ active: labelActive }"
+        :class="{ active: labelActive, isError: isError }"
         @blur="handleBlur"
         @focus="handleFocus"
         @input="handleInput"
@@ -13,44 +13,68 @@
       />
       <label
         class="input-container__placeholder"
-        :class="{ active: labelActive }"
+        :class="{ active: labelActive, isError: isError }"
       >
         <p v-if="labelActive">{{ field.label }}</p>
         <p v-else>{{ field.placeholder }}</p>
       </label>
+      <div
+        class="error-list"
+        v-if="field.errors && Object.keys(field.errors).length"
+      >
+        <p
+          v-for="(error, key) in field.errors"
+          :key="key"
+          class="error-list__item"
+        >
+          {{ error }}
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, ref, watch } from 'vue'
+import { computed, defineComponent, PropType, ref, watch } from 'vue'
+import { FormDataListItem, ValidateParam, ValidateData } from '@/libs/interface'
 
 export default defineComponent({
   name: 'TextInput',
   props: {
     field: {
-      type: Object,
+      type: Object as PropType<FormDataListItem>,
     },
     modelValue: {
-      type: String,
-    },
-    validator: {
       type: String,
     },
     name: {
       type: String,
     },
   },
-  emits: ['update:modelValue'],
+  emits: ['update:modelValue', 'update:validate'],
   setup(props, { emit }) {
     const modelValue = computed(() => props.modelValue)
     const labelActive = ref<boolean>(Boolean(props.modelValue))
+    const isError = ref<boolean>(false)
 
-    // const validate = () => {
-    //   return
-    // }
+    const validate = (text: string) => {
+      const name = props.name
+      const validator = props.field?.validator
+      if (validator && name) {
+        const validateParam: ValidateParam = {
+          key: name,
+          value: text,
+          form: props.field,
+        }
+        const res: ValidateData = validator(validateParam)
+        emit('update:validate', res)
+      }
+    }
 
-    const handleBlur = () => {
+    const handleBlur = (event: any) => {
+      if (event.target.value !== '') {
+        validate(event.target.value)
+      }
       labelActive.value = props.modelValue ? true : false
     }
 
@@ -60,15 +84,25 @@ export default defineComponent({
 
     // any의 사용에 대한 확신이 없음. 수정 필요
     const handleInput = (event: any) => {
+      // modelValue를 먼저 업데이트 해 줘야 validate가 정상적인 로직으로 진행된다.
       emit('update:modelValue', event.target.value)
+      if (props.field?.errors) {
+        validate(event.target.value)
+      }
     }
 
     watch(modelValue, (value) => {
+      // 입력값이 변할 때마다 에러 체크
       labelActive.value = Boolean(value)
+      if (props.field?.errors) {
+        isError.value =
+          Object.keys(props.field?.errors).length === 0 ? false : true
+      }
     })
 
     return {
       labelActive,
+      isError,
       handleBlur,
       handleFocus,
       handleInput,
@@ -82,10 +116,13 @@ export default defineComponent({
   @apply relative w-full;
 
   input {
-    @apply relative text-sm outline-none px-4 py-3 z-10 rounded-md bg-transparent border border-gray-300 w-full py-2;
+    @apply relative mb-1 text-sm outline-none px-4 py-3 z-10 rounded-md bg-transparent border border-gray-300 w-full py-2;
 
     &.active {
       @apply border-indigo-900;
+    }
+    &.isError {
+      @apply border-red-500;
     }
   }
   &__placeholder {
@@ -95,6 +132,14 @@ export default defineComponent({
     &.active {
       @apply bg-white rounded-md text-xs text-indigo-900 p-1 z-10;
       transform: translate(0, -100%);
+    }
+    &.isError {
+      @apply text-red-500;
+    }
+  }
+  .error-list {
+    &__item {
+      @apply text-xs text-red-500;
     }
   }
 }
