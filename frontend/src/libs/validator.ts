15a4a48ace6e2e76a34e74639d598e77c912f7c4
최준원 <cjw1102@naver.com>
2021-09-17 12:36:22 +0900

    feat: Validator module

--- frontend/src/libs/validator.ts
+++ frontend/src/libs/validator.ts
@@ -1,9 +1,61 @@
-interface validateResult {
-  status: boolean
-  message: string
-  check?: boolean
+import { ValidateParam, ValidateData } from '@/libs/interface'
+
+export const emailValidator = (param: ValidateParam): ValidateData => {
+  const key = param.key
+  if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(param.value)) {
+    return {
+      key,
+      type: 'invalidEmail',
+      status: false,
+      message: '이메일 형식이 올바르지 않습니다. 다시 입력해주세요.',
+    }
+  }
+  return {
+    key,
+    type: 'invalidEmail',
+    status: true,
+  }
 }
 
-export const emailValidator = (key: string, value: string): validateResult => {
-  return { status: true, message: 'hi' }
+export const passwordSecurityValidator = (
+  param: ValidateParam
+): ValidateData => {
+  const key = param.key
+  if (
+    !/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/.test(
+      param.value
+    )
+  ) {
+    return {
+      key,
+      type: 'weekPassword',
+      status: true,
+    }
+  }
+  return {
+    key,
+    type: 'weekPassword',
+    status: false,
+    message: '총 8자 이상, 영문/숫자/특수문자 중 2가지 이상 입력해주세요.',
+  }
+}
+
+export const passwordConfirmValidator = (
+  param: ValidateParam
+): ValidateData => {
+  const key = param.key
+  return {
+    key,
+    type: 'passwordConfirmFailed',
+    status: true,
+  }
+}
+
+export const phoneNumberValidator = (param: ValidateParam): ValidateData => {
+  const key = param.key
+  return {
+    key,
+    type: 'invalidPhoneNumber',
+    status: true,
+  }
 }
